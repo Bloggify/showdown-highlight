bb17defa2cb660758268def7d124d8cc14a023a1
Ionică Bizău <bizauionica@gmail.com>
2016-10-30 16:03:14 +0200

    Add tests

--- test/index.js
+++ test/index.js
@@ -2,10 +2,26 @@
 
 const tester = require("tester")
     , showdownHighlight = require("..")
+    , showdown = require('showdown')
     ;
 
 tester.describe("showdown-highlight", t => {
     t.should("A Showdown extension for highlight the code blocks.", () => {
-        t.except(showdownHighlight()).toEqual(/*...*/);
+        // After requiring the module, use it as extension
+        let converter = new showdown.Converter({
+            extensions: [showdownHighlight]
+        });
+
+        // Now you can Highlight code blocks
+        let html = converter.makeHtml(`
+\`\`\`js
+function sayHello (msg, who) {
+    return \`\${who} says: msg\`;
+}
+sayHello("Hello World", "Johnny");
+\`\`\`
+        `);
+
+        t.expect(html.includes("hljs-string")).toEqual(true);
     });
 });
