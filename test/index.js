65ee8583c1dcb3ebd7347834f94ca2fc02eadb88
obedm503 <obedm503@gmail.com>
2018-10-13 20:09:54 -0500

    add failing test

--- test/index.js
+++ test/index.js
@@ -6,12 +6,12 @@ const tester = require("tester")
     ;
 
 tester.describe("showdown-highlight", t => {
-    t.should("A Showdown extension for highlight the code blocks.", () => {
-        // After requiring the module, use it as extension
-        let converter = new showdown.Converter({
-            extensions: [showdownHighlight]
-        });
+    // After requiring the module, use it as extension
+    let converter = new showdown.Converter({
+        extensions: [showdownHighlight]
+    });
 
+    t.should("A Showdown extension for highlight the code blocks.", () => {
         // Now you can Highlight code blocks
         let html = converter.makeHtml(`
 \`\`\`js
@@ -22,7 +22,21 @@ sayHello("Hello World", "Johnny");
 \`\`\`
         `);
 
-        t.expect(html.includes('hljs js language-js')).toEqual(true);
+        t.expect(html.includes('class="hljs js language-js"')).toEqual(true);
         t.expect(html.includes("hljs-string")).toEqual(true);
+    });
+
+    t.should("work without code block language", () => {
+        // Now you can Highlight code blocks
+        let html = converter.makeHtml(`
+\`\`\`
+function sayHello (msg, who) {
+  return \`\${who} says: msg\`;
+}
+sayHello("Hello World", "Johnny");
+\`\`\`
+        `);
+
+        t.expect(html.includes('class="hljs"')).toEqual(true);
     });
 });
