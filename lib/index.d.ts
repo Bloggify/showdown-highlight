428dd703b0b0163c3b685fc8c69ded2e2d612e91
gfnuko <xahhaepica@gmail.com>
2022-09-18 00:14:02 +0900

    Add an option to control hljs' auto detection

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,4 @@
 import type { ShowdownExtension } from "showdown";
 
-declare function showdownHighlight({ pre: Boolean }): ShowdownExtension[];
+declare function showdownHighlight({ pre: Boolean, auto_detection: Boolean }): ShowdownExtension[];
 export = showdownHighlight;
